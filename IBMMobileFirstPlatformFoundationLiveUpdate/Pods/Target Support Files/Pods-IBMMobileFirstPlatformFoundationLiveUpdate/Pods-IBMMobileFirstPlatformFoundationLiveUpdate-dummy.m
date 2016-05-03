967474f6dce9f432b102dda97a05889189ea4c35
ishaib <ishaib@il.ibm.com>
2016-05-03 14:55:28 +0300

    Remove pod

--- IBMMobileFirstPlatformFoundationLiveUpdate/Pods/Target Support Files/Pods-IBMMobileFirstPlatformFoundationLiveUpdate/Pods-IBMMobileFirstPlatformFoundationLiveUpdate-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IBMMobileFirstPlatformFoundationLiveUpdate : NSObject
-@end
-@implementation PodsDummy_Pods_IBMMobileFirstPlatformFoundationLiveUpdate
-@end
