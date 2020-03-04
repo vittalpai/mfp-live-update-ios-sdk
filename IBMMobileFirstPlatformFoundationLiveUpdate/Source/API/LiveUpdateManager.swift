fdf9a1e0a0a84b1fa89b8fe6b6275194f109991d
Vittal Pai <vittalpai@in.ibm.com>
2020-03-04 17:19:15 +0530

    Expose API for default segment

--- IBMMobileFirstPlatformFoundationLiveUpdate/Source/API/LiveUpdateManager.swift
+++ IBMMobileFirstPlatformFoundationLiveUpdate/Source/API/LiveUpdateManager.swift
@@ -64,6 +64,25 @@ open class LiveUpdateManager {
         }
     }
 
+     /**
+     Obtains a configuration from server / cache
+     
+     - Parameter completionHandler - the competition for retrieving the Configuration
+     */
+    open func obtainConfiguration (completionHandler: @escaping (_ configuration: Configuration?, _ error: NSError?) -> Void) {
+        let encodedSegment = ecodeString("all")
+        let urlString = applicationRoute +  serviceURL + "/\(encodedSegment!)"
+        if isValidUrl(url: urlString) {
+            let url = URL(string: urlString)!
+        OCLogger.getLogger().logDebugWithMessages("obtainConfiguration: segment = \(String(describing: "all")), useCache = true, url = \(url)")
+        self.obtainConfiguration("all", url: url, params: nil, useCache: true, completionHandler: completionHandler)
+        } else {
+            let error = "Failed to initialize Liveupdate instance because server url is nil. Check the server details in mfpclient.plist"
+            OCLogger.getLogger().logDebugWithMessages(error)
+            completionHandler(nil, NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : error]))
+    }
+    }
+
     /**
      Obtains a configuration from server / cache
      
